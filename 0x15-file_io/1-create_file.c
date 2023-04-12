11575a37404734c5a1216fbc0e7c1b1571710aaa
Be Bulinda <hello@reflexconcepts.co.ke>
2023-04-12 20:08:45 +0300

    Error Correction.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,24 +11,24 @@
  */
 int append_text_to_file(const char* filename, char* text_content)
 {
-	int o, w, len = 0;
+	int ox, wy, length = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		for (length = 0; text_content[len];)
+			length++;
 	}
 
-	o = open(filename, O_WRONLY | O_APPEND);
-	w = write(o, text_content, len);
+	ox = open(filename, O_WRONLY | O_APPEND);
+	wy = write(ox, text_content, length);
 
-	if (o == -1 || w == -1)
+	if (ox == -1 || wy == -1)
 		return (-1);
 
-	close(o);
+	close(ox);
 
 	return (1);
 }
